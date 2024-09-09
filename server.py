a0d902fd8160878a310bd4abdb365c4868541cb0
LouisDeMilleville <125300400+LouisDeMilleville@users.noreply.github.com>
2024-09-09 12:14:40 +0200

    Adding the /reply endpoint

--- server.py
+++ server.py
@@ -50,6 +50,20 @@ def loadCookies(driver, path_cookies):
     driver.get('https://chatgpt.com/')
     print("Cookies loaded")
 
+# Function to get the id of the last chat we used
+def get_chat_id(driver):
+    try:
+        # First, we locate the button to the chat
+        chat_element = driver.find_element(By.XPATH, '//a[contains(@class, "flex items-center gap-2 p-2")]')
+        # Then we save the content of the href attribute which contains the id of our chat
+        href = chat_element.get_attribute('href')
+        # Finally, we only keep the id since we don't need a url
+        chat_id = href.split('/c/')[1]
+        return chat_id
+    except Exception as e:
+        print(f"An error occurred while getting the chat ID: {e}")
+        return None
+
 # Function to detect when the answer is fully generated
 def wait_for_assistant_response(driver):
     while True:
@@ -62,11 +76,15 @@ def wait_for_assistant_response(driver):
                 # When the answer is completed, we extract it from the page
                 elements = driver.find_elements(By.XPATH, '//div[@data-message-author-role="assistant"]')
                 
-                if len(elements) > 0:
-                    if len(elements) == 1:
-                        return elements[0].text
-                    elif len(elements) == 2:
-                        return f"Answer 1 : {elements[0].text}\nAnswer 2 : {elements[1].text}"
+                # We save the chat id so we can use it later
+                chat_id = get_chat_id(driver)
+                answer_id = len(elements) - 1
+                return f"Chat id : {chat_id}\nAnswer : {elements[answer_id].text}"
+                #if len(elements) > 0:
+                #    if len(elements) == 1:
+                #        return f"Chat id : {chat_id}\nAnswer : {elements[0].text}"
+                #    elif len(elements) == 2:
+                #        return f"Answer 1 : {elements[0].text}\nAnswer 2 : {elements[1].text}"
                 break
             else:
                 # If there's no send button, it means the answer is still being generated so we wait before checking again
@@ -93,6 +111,51 @@ def ask():
 
     # Then we reload chatGPT to make sure we're on a new chat
     driver.get('https://chatgpt.com/')
+    time.sleep(3)
+    
+    # Then we paste the prompt on the text area and we send it
+    textarea_xpath = "/html/body/div[1]/div[2]/main/div[1]/div[2]/div/div[1]/div/form/div/div[2]/div/div/div[2]/textarea"
+    try:
+        element = WebDriverWait(driver, 10).until(
+            EC.visibility_of_element_located((By.XPATH, textarea_xpath))
+        ) 
+        element.send_keys(prompt)
+        element.send_keys(Keys.RETURN)
+        time.sleep(5)
+        response = wait_for_assistant_response(driver)
+        # Again, we make sure the answer sent to the user is also in utf8
+        response_utf8 = response.encode('utf-8').decode('utf-8')
+        return make_response(response_utf8, 200)
+        
+    except Exception as e:
+        print(e)
+        return make_response(str(e), 500)
+        
+# The endpoint to reply to an existing chat by providing it's id
+@app.route('/reply', methods=['POST'])
+def reply():
+    global driver
+    if not driver:
+        return make_response("Driver not initialized", 500)
+
+    chat_id = request.headers.get('Chat_id')
+    print(f"Chat_id: {chat_id}")
+    print(f"Request headers:\n{request.headers}")
+    # First we get the prompt from the 'Prompt' header
+    prompt = request.headers.get('Prompt')
+    if not prompt:
+        return make_response("Prompt not provided", 400)
+    # Then we save the chat id
+    chat_id = request.headers.get('Chat-id')
+    if not chat_id:
+        return make_response("Chat_id not provided. If it's intentional, you should use the /ask endpoint instead")
+
+    # We make sure the prompt is in utf8
+    prompt = prompt.encode('latin1').decode('utf-8')
+
+    # Then we reload chatGPT to make sure we're on a new chat
+    chat_url = 'https://chatgpt.com/c/' + str(chat_id)
+    driver.get(chat_url)
     time.sleep(3)
     
     # Then we paste the prompt on the text area and we send it
